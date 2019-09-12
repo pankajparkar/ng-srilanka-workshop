1d264219ff9d7cf1f0042e0a74b792e9d122317c
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-09-13 00:33:15 +0530

    Added cards for each album.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'n-things-that-angular-dev-should-know';
+  albums;
+  constructor (private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('https://jsonplaceholder.typicode.com/albums').subscribe(
+      data => this.albums = data
+    )
+  }
 }
