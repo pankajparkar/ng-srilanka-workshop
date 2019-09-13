ad20b4059f156c6004b5e4821a64089fa42f4b33
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-09-13 12:53:09 +0530

    Added all 4 ajax call.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,23 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   title = 'n-things-that-angular-dev-should-know';
   albums;
+  photos;
+  album;
+  photo;
   constructor (private http: HttpClient) { }
 
   ngOnInit() {
     this.http.get('https://jsonplaceholder.typicode.com/albums').subscribe(
       data => this.albums = data
+    )
+    this.http.get('https://jsonplaceholder.typicode.com/albums').subscribe(
+      data => this.album = data
+    )
+    this.http.get('https://jsonplaceholder.typicode.com/photos').subscribe(
+      data => this.photos = data
+    )
+    this.http.get('https://jsonplaceholder.typicode.com/photos/1').subscribe(
+      data => this.photo = data
     )
   }
 }
