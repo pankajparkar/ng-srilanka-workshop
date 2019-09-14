1c4509082e9e1ad1e8b9a4918b14b1aef383916c
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-09-14 06:34:07 +0530

    Added movie dummy data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'App component works!';
+  movie = {"Title":"The Rock","Year":"1996","imdbID":"tt0117500","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BZDJjOTE0N2EtMmRlZS00NzU0LWE0ZWQtM2Q3MWMxNjcwZjBhXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg"};
+
   constructor () { }
 
   ngOnInit() {
